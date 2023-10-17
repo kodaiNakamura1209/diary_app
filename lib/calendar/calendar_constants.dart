0a3d2bdf824c5a82306db1dfa9a26637ea01a053
kodaiNakamura1209 <natural.green1209@ezweb.ne.jp>
2023-10-17 21:35:06 +0900

    ・カレンダーの日付部分を作成するメソッドを作成

---
+++ lib/calendar/calendar_constants.dart
@@ -0,0 +1,21 @@
+class CalendarConstants{
+  // 表示用年月
+  static const String dispYear = "年";
+  static const String dispMonth = "月";
+  // 表示用曜日
+  static const String dispSunday = "日";
+  static const String dispMonday = "月";
+  static const String dispTuesday = "火";
+  static const String dispWednesday = "水";
+  static const String dispThursday = "木";
+  static const String dispFriday = "金";
+  static const String dispSaturday = "土";
+  // 曜日(int)
+  static const int Sunday = 7;
+  static const int Monday = 1;
+  static const int Tuesday = 2;
+  static const int Wednesday = 3;
+  static const int Thursday = 4;
+  static const int Friday = 5;
+  static const int Saturday = 6;
+}
--- lib/calendar/calendar_table.dart
+++ lib/calendar/calendar_table.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'calendar_constants.dart';
 import 'date_block.dart';
 
 class CalendarTable extends StatelessWidget {
@@ -9,6 +10,10 @@ class CalendarTable extends StatelessWidget {
   late final int startMonth;
   // テーブル生成対象年月(Date)
   late final DateTime _startDate;
+  // 空欄のwidget
+  final Widget SPACER = const Spacer();
+  // 日付加算用の期間クラス(1日間を表している)
+  final Duration duration = const Duration(days:1);
 
   // コンストラクタ
   // startYear・・・対象年
@@ -24,7 +29,7 @@ class CalendarTable extends StatelessWidget {
           child: Column(
             children: [
               Text(
-                  startYear.toString()+"年"+startMonth.toString()+"月"
+                  startYear.toString()+CalendarConstants.dispYear+startMonth.toString()+CalendarConstants.dispMonth
               ),
               CreateTable(),
               // Table(
@@ -107,24 +112,134 @@ class CalendarTable extends StatelessWidget {
   Table CreateTable(){
     return Table(
       border: TableBorder.all(),
-      children: CreateTableRow(),
+      children: CreateTableRow(startYear,startMonth),
     );
   }
 
-  List<TableRow> CreateTableRow(){
+  // テーブル行生成メソッド
+  List<TableRow> CreateTableRow(int year, int month){
+
+    // テーブル行リスト
     List<TableRow> tableRowList = [];
+    // カレンダー日付
+    DateTime calendarDay = DateTime(year,month,1);
+    // 曜日格納変数
+    int weekday = calendarDay.weekday;
+
     // ヘッダー部を生成
     tableRowList.add(const TableRow(children: [
-      Text("日",textAlign: TextAlign.center),
-      Text("月",textAlign: TextAlign.center),
-      Text("火",textAlign: TextAlign.center),
-      Text("水",textAlign: TextAlign.center),
-      Text("木",textAlign: TextAlign.center),
-      Text("金",textAlign: TextAlign.center),
-      Text("土",textAlign: TextAlign.center)
+      Text(CalendarConstants.dispSunday,textAlign: TextAlign.center),
+      Text(CalendarConstants.dispMonday,textAlign: TextAlign.center),
+      Text(CalendarConstants.dispTuesday,textAlign: TextAlign.center),
+      Text(CalendarConstants.dispWednesday,textAlign: TextAlign.center),
+      Text(CalendarConstants.dispThursday,textAlign: TextAlign.center),
+      Text(CalendarConstants.dispFriday,textAlign: TextAlign.center),
+      Text(CalendarConstants.dispSaturday,textAlign: TextAlign.center)
     ]));
 
     // for文でカレンダーを作成してListにつめる
+    for(int i = 0; i < 5; i++){
+
+      // 1行目は曜日を考慮して設定を行う
+      if (i == 0){
+        switch(weekday){
+        // 月の初めが日曜日の場合
+          case CalendarConstants.Sunday:
+            tableRowList.add(TableRow(children: [
+              DateBlock(calendarDay.day, null),
+            ]));
+            break;
+        // 月の初めが月曜日の場合
+          case CalendarConstants.Monday:
+            tableRowList.add(TableRow(children: [
+              SPACER,
+              DateBlock(calendarDay.day, null),
+            ]));
+            break;
+        // 月の初めが火曜日の場合
+          case CalendarConstants.Tuesday:
+            tableRowList.add(TableRow(children: [
+              SPACER,
+              SPACER,
+              DateBlock(calendarDay.day, null),
+            ]));
+            break;
+        // 月の初めが水曜日の場合
+          case CalendarConstants.Wednesday:
+            tableRowList.add(TableRow(children: [
+              SPACER,
+              SPACER,
+              SPACER,
+              DateBlock(calendarDay.day, null),
+            ]));
+            break;
+        // 月の初めが木曜日の場合
+          case CalendarConstants.Thursday:
+            tableRowList.add(TableRow(children: [
+              SPACER,
+              SPACER,
+              SPACER,
+              SPACER,
+              DateBlock(calendarDay.day, null),
+            ]));
+            break;
+        // 月の初めが金曜日の場合
+          case CalendarConstants.Friday:
+            tableRowList.add(TableRow(children: [
+              SPACER,
+              SPACER,
+              SPACER,
+              SPACER,
+              SPACER,
+              DateBlock(calendarDay.day, null),
+            ]));
+            break;
+        // 月の初めが土曜日の場合
+          case CalendarConstants.Saturday:
+            tableRowList.add(TableRow(children: [
+              SPACER,
+              SPACER,
+              SPACER,
+              SPACER,
+              SPACER,
+              SPACER,
+              DateBlock(calendarDay.day, null),
+            ]));
+            break;
+        }
+        // カレンダー日付を次の日にする
+        calendarDay = calendarDay.add(duration);
+      } else {
+        // 2行目以降
+        List<Widget> dateBlockList = [];
+
+        for(int j = 0; j < 7; j++){
+          // 最終行以外の場合
+          if (i != 4){
+            // 最終行以外の場合
+            // 日付をDataBlockのリストに入れる
+            dateBlockList.add(DateBlock(calendarDay.day, null));
+            // カレンダー日付を次の日にする
+            calendarDay = calendarDay.add(duration);
+          } else {
+            // 最終行の場合
+            // カレンダーの月が変わっていたら空widgetを入れる
+            if(month != calendarDay.month){
+              dateBlockList.add(SPACER);
+            } else {
+              // 日付をDataBlockのリストに入れる
+              dateBlockList.add(DateBlock(calendarDay.day, null));
+              // カレンダー日付を次の日にする
+              calendarDay = calendarDay.add(duration);
+            }
+          }
+        }
+
+        // 日付ブロックwidgetリストをテーブル行widgetリストに格納する
+        tableRowList.add(TableRow(children: dateBlockList));
+      }
+    }
+
     return tableRowList;
   }
 }
