0b6bc54f029d8af70035bb0d067eec9dca85339e
kodaiNakamura1209 <natural.green1209@ezweb.ne.jp>
2023-10-24 22:12:28 +0900

    ・カレンダーを自動生成して表示する処理を作成

--- lib/calendar/calendar.dart
+++ lib/calendar/calendar.dart
@@ -2,16 +2,56 @@ import 'package:flutter/material.dart';
 import 'calendar_table.dart';
 
 class Calendar extends StatelessWidget {
-  final DateTime _startDay = DateTime(2023, 10, 1);
+  late final DateTime _startYearMonth = DateTime(2023, 10, 1);
+  late final DateTime _endYearMonth = DateTime(2024, 10, 1);
 
   @override
   Widget build(BuildContext context) {
     return PageView(
-      children: [
-        CalendarTable(2023,10),
-        CalendarTable(2023,11),
-        CalendarTable(2023,12),
-      ],
+      children: CreateCalendarWiget(_startYearMonth, _endYearMonth),
+      // children: [
+      //   CalendarTable(2023,10),
+      //   CalendarTable(2023,11),
+      // ],
     );
+  }
+
+  // カレンダーWighet生成メソッド
+  List<Widget>CreateCalendarWiget(DateTime startYearMonth,DateTime endYearMonth){
+    print('カレンダー生成開始');
+    // カレンダーテーブル格納リスト
+    List<Widget> calendarTableList = [];
+
+    // 入力チェックが問題なければ処理を実施
+    if(inputCheck(startYearMonth,endYearMonth)){
+
+      // 生成年月制御用変数
+      int startYear = startYearMonth.year;
+      int startMonth = startYearMonth.month;
+      int intEndYearMonth = (endYearMonth.year * 100) + endYearMonth.month;
+
+      // 開始年月が終了年月を超えるまで繰り返す
+      while(intEndYearMonth >= (startYear * 100) + startMonth ){
+
+        // カレンダーテーブルリストに現在のひと月分のカレンダーテーブルを追加
+        calendarTableList.add(CalendarTable(startYear, startMonth));
+        // 開始月を翌月にする
+        ++startMonth;
+        // 開始月が12を超えた場合、開始年と開始月を次の年の1月に変更する
+        if(startMonth > 12){
+          startYear += 1;
+          startMonth = 1;
+        }
+      }
+    }else{
+
+    }
+    print('カレンダー生成終了');
+    return calendarTableList;
+  }
+
+  // 入力チェックメソッド
+  bool inputCheck(DateTime startYearMonth,DateTime endYearMonth){
+    return true;
   }
 }
--- lib/calendar/calendar_table.dart
+++ lib/calendar/calendar_table.dart
@@ -32,77 +32,6 @@ class CalendarTable extends StatelessWidget {
                   startYear.toString()+CalendarConstants.dispYear+startMonth.toString()+CalendarConstants.dispMonth
               ),
               CreateTable(),
-              // Table(
-              //   border: TableBorder.all(),
-              //   children: [
-              //     const TableRow(
-              //         children: [
-              //           Text("日",textAlign: TextAlign.center),
-              //           Text("月",textAlign: TextAlign.center),
-              //           Text("火",textAlign: TextAlign.center),
-              //           Text("水",textAlign: TextAlign.center),
-              //           Text("木",textAlign: TextAlign.center),
-              //           Text("金",textAlign: TextAlign.center),
-              //           Text("土",textAlign: TextAlign.center)
-              //         ]
-              //     ),
-              //     TableRow(
-              //         children: [
-              //           DateBlock(_startDate.day, Icons.book_rounded),
-              //           DateBlock(_startDate.day,null),
-              //           DateBlock(_startDate.day, Icons.book_rounded),
-              //           DateBlock(_startDate.day,null),
-              //           DateBlock(_startDate.day, Icons.book_rounded),
-              //           DateBlock(_startDate.day,null),
-              //           DateBlock(_startDate.day, Icons.book_rounded),
-              //         ]
-              //     ),
-              //     TableRow(
-              //         children: [
-              //           DateBlock(_startDate.day, Icons.book_rounded),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //         ]
-              //     ),
-              //     TableRow(
-              //         children: [
-              //           DateBlock(_startDate.day, Icons.book_rounded),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //         ]
-              //     ),
-              //     TableRow(
-              //         children: [
-              //           DateBlock(_startDate.day, Icons.book_rounded),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //         ]
-              //     ),
-              //     TableRow(
-              //         children: [
-              //           DateBlock(_startDate.day, Icons.book_rounded),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //           Spacer(),
-              //         ]
-              //     ),
-              //   ],
-              // )
             ],
           ),
         )
@@ -138,105 +67,118 @@ class CalendarTable extends StatelessWidget {
     ]));
 
     // for文でカレンダーを作成してListにつめる
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < 6; i++){
+
+      // 2行目以降
+      List<Widget> dateBlockList = [];
 
       // 1行目は曜日を考慮して設定を行う
       if (i == 0){
         switch(weekday){
-        // 月の初めが日曜日の場合
+          // 月の初めが日曜日の場合
           case CalendarConstants.Sunday:
-            tableRowList.add(TableRow(children: [
-              DateBlock(calendarDay.day, null),
-            ]));
-            break;
-        // 月の初めが月曜日の場合
-          case CalendarConstants.Monday:
-            tableRowList.add(TableRow(children: [
-              SPACER,
-              DateBlock(calendarDay.day, null),
-            ]));
-            break;
-        // 月の初めが火曜日の場合
-          case CalendarConstants.Tuesday:
-            tableRowList.add(TableRow(children: [
-              SPACER,
-              SPACER,
-              DateBlock(calendarDay.day, null),
-            ]));
-            break;
-        // 月の初めが水曜日の場合
-          case CalendarConstants.Wednesday:
-            tableRowList.add(TableRow(children: [
-              SPACER,
-              SPACER,
-              SPACER,
-              DateBlock(calendarDay.day, null),
-            ]));
-            break;
-        // 月の初めが木曜日の場合
-          case CalendarConstants.Thursday:
-            tableRowList.add(TableRow(children: [
-              SPACER,
-              SPACER,
-              SPACER,
-              SPACER,
-              DateBlock(calendarDay.day, null),
-            ]));
-            break;
-        // 月の初めが金曜日の場合
-          case CalendarConstants.Friday:
-            tableRowList.add(TableRow(children: [
-              SPACER,
-              SPACER,
-              SPACER,
-              SPACER,
-              SPACER,
-              DateBlock(calendarDay.day, null),
-            ]));
-            break;
-        // 月の初めが土曜日の場合
-          case CalendarConstants.Saturday:
-            tableRowList.add(TableRow(children: [
-              SPACER,
-              SPACER,
-              SPACER,
-              SPACER,
-              SPACER,
-              SPACER,
-              DateBlock(calendarDay.day, null),
-            ]));
-            break;
-        }
-        // カレンダー日付を次の日にする
-        calendarDay = calendarDay.add(duration);
-      } else {
-        // 2行目以降
-        List<Widget> dateBlockList = [];
-
-        for(int j = 0; j < 7; j++){
-          // 最終行以外の場合
-          if (i != 4){
-            // 最終行以外の場合
-            // 日付をDataBlockのリストに入れる
-            dateBlockList.add(DateBlock(calendarDay.day, null));
-            // カレンダー日付を次の日にする
-            calendarDay = calendarDay.add(duration);
-          } else {
-            // 最終行の場合
-            // カレンダーの月が変わっていたら空widgetを入れる
-            if(month != calendarDay.month){
-              dateBlockList.add(SPACER);
-            } else {
-              // 日付をDataBlockのリストに入れる
+            for(int j = 0; j < 7; j++){
               dateBlockList.add(DateBlock(calendarDay.day, null));
               // カレンダー日付を次の日にする
               calendarDay = calendarDay.add(duration);
             }
+            tableRowList.add(TableRow(children: dateBlockList));
+            break;
+          // 月の初めが月曜日の場合
+          case CalendarConstants.Monday:
+            dateBlockList.add(SPACER);
+            for(int j = 0; j < 6; j++){
+              dateBlockList.add(DateBlock(calendarDay.day, null));
+              // カレンダー日付を次の日にする
+              calendarDay = calendarDay.add(duration);
+            }
+            tableRowList.add(TableRow(children: dateBlockList));
+            break;
+          // 月の初めが火曜日の場合
+          case CalendarConstants.Tuesday:
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            for(int j = 0; j < 5; j++){
+              dateBlockList.add(DateBlock(calendarDay.day, null));
+              // カレンダー日付を次の日にする
+              calendarDay = calendarDay.add(duration);
+            }
+            tableRowList.add(TableRow(children: dateBlockList));
+            break;
+          // 月の初めが水曜日の場合
+          case CalendarConstants.Wednesday:
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            for(int j = 0; j < 4; j++){
+              dateBlockList.add(DateBlock(calendarDay.day, null));
+              // カレンダー日付を次の日にする
+              calendarDay = calendarDay.add(duration);
+            }
+            tableRowList.add(TableRow(children: dateBlockList));
+            break;
+          // 月の初めが木曜日の場合
+          case CalendarConstants.Thursday:
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            for(int j = 0; j < 3; j++){
+              dateBlockList.add(DateBlock(calendarDay.day, null));
+              // カレンダー日付を次の日にする
+              calendarDay = calendarDay.add(duration);
+            }
+            tableRowList.add(TableRow(children: dateBlockList));
+            break;
+          // 月の初めが金曜日の場合
+          case CalendarConstants.Friday:
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            for(int j = 0; j < 2; j++){
+              dateBlockList.add(DateBlock(calendarDay.day, null));
+              // カレンダー日付を次の日にする
+              calendarDay = calendarDay.add(duration);
+            }
+            tableRowList.add(TableRow(children: dateBlockList));
+            break;
+          // 月の初めが土曜日の場合
+          case CalendarConstants.Saturday:
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            dateBlockList.add(SPACER);
+            dateBlockList.add(DateBlock(calendarDay.day, null));
+            // カレンダー日付を次の日にする
+            calendarDay = calendarDay.add(duration);
+            tableRowList.add(TableRow(children: dateBlockList));
+            break;
+        }
+      } else {
+        // 1行目以外の場合
+        for(int j = 0; j < 7; j++){
+          // カレンダーの月が変わっていたら空widgetを入れる
+          if(month != calendarDay.month){
+            dateBlockList.add(SPACER);
+          } else {
+            // 日付をDataBlockのリストに入れる
+            dateBlockList.add(DateBlock(calendarDay.day, null));
+            // カレンダー日付を次の日にする
+            calendarDay = calendarDay.add(duration);
           }
         }
 
         // 日付ブロックwidgetリストをテーブル行widgetリストに格納する
         tableRowList.add(TableRow(children: dateBlockList));
+      }
+
+      // カレンダーの月が変わっていた場合、処理を終了する
+      if(month != calendarDay.month){
+        break;
       }
     }
 
